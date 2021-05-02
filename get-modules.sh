085818a98297fbf6f2d1abab03a9ea5057f54aef
xyzroller <xyzroller@rollyourown.xyz>
2021-05-02 14:21:32 +0200

    Added destination paths to get-modules script

--- get-modules.sh
+++ get-modules.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ## Common to all projects - generic host setup repository
-git submodule add https://git.rollyourown.xyz/ryo-projects/ryo-host-setup-generic
+git submodule add https://git.rollyourown.xyz/ryo-projects/ryo-host-setup-generic modules/ryo-host-setup-generic
 
 ## Project specific submodules
-#git submodule add https://git.rollyourown.xyz/ryo-projects/<PROJECT MODULE>
-#git submodule add https://git.rollyourown.xyz/ryo-projects/<PROJECT MODULE>
+#git submodule add https://git.rollyourown.xyz/ryo-projects/<PROJECT MODULE> modules/<PROJECT MODULE>
+#git submodule add https://git.rollyourown.xyz/ryo-projects/<PROJECT MODULE> modules/<PROJECT MODULE>
