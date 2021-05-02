c30c45b86171151c223912f6a276a7cf963ce34e
xyzroller <xyzroller@rollyourown.xyz>
2021-05-02 16:46:31 +0200

    Added ryo-haproxy-consul-certbot in get-modules

--- get-modules.sh
+++ get-modules.sh
@@ -7,6 +7,10 @@ echo ""
 echo "Executing 'git submodule add' for ryo-host-setup-generic repository"
 git submodule add https://git.rollyourown.xyz/ryo-projects/ryo-host-setup-generic modules/ryo-host-setup-generic
 
+## Common to almost all projects - loadbalancer / TLS proxy repository
+echo "Executing 'git submodule add' for ryo-haproxy-consul-certbot repository"
+git submodule add https://git.rollyourown.xyz/ryo-projects/ryo-haproxy-consul-certbot modules/ryo-haproxy-consul-certbot
+
 ## Project specific submodules
 #echo "Executing 'git submodule add' for <PROJECT MODULE> repository"
 #git submodule add https://git.rollyourown.xyz/ryo-projects/<PROJECT MODULE> modules/<PROJECT MODULE>
