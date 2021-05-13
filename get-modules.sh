72e055ea31fa8d2ad1ae7ed68ccb28a57fae61a6
xyzroller <xyzroller@rollyourown.xyz>
2021-05-13 12:32:40 +0200

    Added conul module to get-modules script

--- get-modules.sh
+++ get-modules.sh
@@ -7,6 +7,10 @@ echo ""
 echo "Executing 'git submodule add' for ryo-host-setup-generic repository"
 git submodule add https://git.rollyourown.xyz/ryo-projects/ryo-host-setup-generic modules/ryo-host-setup-generic
 
+## Common to almost all projects - service registry / KV store
+echo "Executing 'git submodule add' for ryo-service-registry-kv-store"
+git submodule add https://git.rollyourown.xyz/ryo-projects/ryo-service-registry-kv-store modules/ryo-service-registry-kv-store
+
 ## Common to almost all projects - loadbalancer / TLS proxy repository
 echo "Executing 'git submodule add' for ryo-loadbalancer-tls-proxy repository"
 git submodule add https://git.rollyourown.xyz/ryo-projects/ryo-loadbalancer-tls-proxy modules/ryo-loadbalancer-tls-proxy
