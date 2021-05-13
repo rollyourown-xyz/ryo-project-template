e7b6723ea380417fb511cc30360f10276a46391a
xyzroller <xyzroller@rollyourown.xyz>
2021-05-13 15:12:42 +0200

    Added consul and haproxy/certbot to template

--- host-setup.sh
+++ host-setup.sh
@@ -7,6 +7,18 @@ echo ""
 echo "Executing generic host setup playbooks"
 ansible-playbook -i configuration/inventory modules/ryo-host-setup-generic/host-setup/master.yml
 
+# Common to almost all projects - Module-specific host setup for ryo-service-registry-kv-store
+echo "Executing module-specific host setup playbooks for ryo-service-registry-kv-store"
+ansible-playbook -i configuration/inventory modules/ryo-service-registry-kv-store/host-setup-module/master.yml
+
+# Common to almost all projects - Module-specific host setup for ryo-loadbalancer-tls-proxy
+echo "Executing module-specific host setup playbooks for ryo-loadbalancer-tls-proxy"
+ansible-playbook -i configuration/inventory modules/ryo-loadbalancer-tls-proxy/host-setup-module/master.yml
+
+## Other module-specific host setup here...
+
+
+
 # Project-specific host setup
 echo "Executing project-specific host setup playbooks"
 ansible-playbook -i configuration/inventory host-setup-project/master.yml
