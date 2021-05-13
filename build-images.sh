820133fb59a1d06a396625b36461884f8a6b4d50
xyzroller <xyzroller@rollyourown.xyz>
2021-05-13 15:12:10 +0200

    Removed grav_version and added further comments

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Add additional help for software package versions defined below
 helpMessage()
 {
    echo "build-images.sh: Use packer to build images for deployment"
@@ -9,7 +10,6 @@ helpMessage()
    echo "Flags:"
    echo -e "-v version \t\t(Mandatory) Version stamp to apply to images, e.g. 20210101-1"
    echo -e "-w webhook_version \t(Optional) Webhook version to use for the haproxy-dmz image, e.g. 2.8.0 (default)"
-   echo -e "-g grav_version \t(Optional) Grav version to use for the webserver image, e.g. 1.7.10 (default)"
    echo -e "-h \t\t\tPrint this help message"
    echo ""
    exit 1
@@ -22,28 +22,39 @@ errorMessage()
    exit 1
 }
 
-# Default webhook and grav versions
+# Default software package versions
+# Add software package versions here, for example webhook, needed for loadbalancer / TLS proxy module
 webhook_version=2.8.0
-grav_version=1.7.13
 
-while getopts v:w:g:h flag
+# Add additional options for software package versions defined above
+while getopts v:h flag
 do
     case "${flag}" in
         v) version=${OPTARG};;
         w) webhook_version=${OPTARG};;
-        g) grav_version=${OPTARG};;
         h) helpMessage ;;
         ?) errorMessage ;;
     esac
 done
 
-if [ -z "$version" ] || [ -z "$webhook_version" ] || [ -z "$grav_version" ]
+if [ -z "$version" ] || [ -z "$webhook_version" ]
 then
    errorMessage
 fi
 
-echo "Building images with version $version, Webhook version $webhook_version and Grav version $grav_version"
+# Add additional software package versions defined above
+echo "Building images with version $version, Webhook version $webhook_version"
 echo ""
+
+# Common to almost all projects - service registry and key-value store module
+# Remove if not needed for this project
+echo "Building Consul image"
+echo "Executing command: packer build -var \"version=$version\" modules/ryo-service-registry-kv-store/image-build/consul.pkr.hcl"
+echo ""
+packer build -var "version=$version" modules/ryo-service-registry-kv-store/image-build/consul.pkr.hcl
+
+# Common to almost all projects - loadbalancer / TLS proxy with certificate management module
+# Remove if not needed for this project
 echo "Building HAProxy image"
 echo "Executing command: packer build -var \"version=$version\" -var \"webhook_version=$webhook_version\" image-build/haproxy-dmz.pkr.hcl"
 echo ""
@@ -54,9 +65,12 @@ echo "Executing command: packer build -var \"version=$version\" certbot.pkr.hcl"
 echo ""
 packer build -var "version=$version" image-build/certbot.pkr.hcl
 echo ""
-echo "Building webserver image"
-echo "Executing command: packer build -var \"version=$version\" -var \"grav_version=$grav_version\" webserver.pkr.hcl"
-echo ""
-packer build -var "version=$version" -var "grav_version=$grav_version" image-build/webserver.pkr.hcl
-echo ""
+
+# Project-specific image builds here..., for example
+#echo "Building webserver image"
+#echo "Executing command: packer build -var \"version=$version\" -var \"grav_version=$grav_version\" webserver.pkr.hcl"
+#echo ""
+#packer build -var "version=$version" -var "grav_version=$grav_version" image-build/webserver.pkr.hcl
+#echo ""
+
 echo "Completed"
