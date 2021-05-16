026e3d8ff2211e233c76337d23d4314001319297
xyzroller <xyzroller@rollyourown.xyz>
2021-05-16 10:48:02 +0200

    Added build and deployment resources for modules

--- project-deployment/xx-TEMPLATE.tf
+++ project-deployment/xx-TEMPLATE.tf
@@ -1,4 +1,18 @@
-## TEMPLATE TERRAFORM FILE FOR DEPLOYMENT OF A COMPONENT
+## TERRAFORM FILE EXAMPLES FOR DEPLOYMENT OF A COMPONENT
+
+# Deployment of certificate domains for certbot for the project
+###############################################################
+# module "<COMPONENT_NAME>-cert-domains" {
+#   source = "./modules/deploy-cert-domains"
+
+#   depends_on = [ module.deploy-consul ]
+
+#   certificate_domains = {
+#     domain_1 = {domain = local.project_domain_name, admin_email = local.project_admin_email},
+#     domain_2 = {domain = join("", [ "www.", local.project_domain_name]), admin_email = local.project_admin_email}
+#   }
+# }
+
 
 # Deployment of <COMPONENT_NAME> using a static IP address and a plain cloud-init file
 ######################################################################################
@@ -6,12 +20,14 @@
 # module "deploy-<COMPONENT_NAME>" {
 #   source = "./modules/deploy-container-static-ip"
 
+#   depends_on = [ module.deploy-consul ]
+  
 #   lxd_remote                 = local.lxd_remote_name
 #   host_external_ipv4_address = local.lxd_host_public_ipv4_address
 #   container_image            = join("-", [ local.project_id, "COMPONENT_NAME", var.image_version ])
 #   container_name             = join("-", [ local.project_id, "COMPONENT_NAME" ])
 #   container_profiles         = ["default"]
-#   container_network          = "LXD NETWORK TO USE"
+#   container_network          = local.project_id
 #   container_ipv4_address     = join(".", [ local.LXD_NETWORK_TO_USE, "10" ])
 
 #   container_cloud-init       = file("cloud-init/cloud-TEMPLATE.yml")
@@ -35,12 +51,14 @@
 # module "deploy-<COMPONENT_NAME>" {
 #   source = "./modules/deploy-container-static-ip"
 
+#   depends_on = [ module.deploy-consul ]
+
 #   lxd_remote                 = local.lxd_remote_name
 #   host_external_ipv4_address = local.lxd_host_public_ipv4_address
 #   container_image            = join("-", [ local.project_id, "COMPONENT_NAME", var.image_version ])
 #   container_name             = join("-", [ local.project_id, "COMPONENT_NAME" ])
 #   container_profiles         = ["default"]
-#   container_network          = "LXD NETWORK TO USE"
+#   container_network          = local.project_id
 #   container_ipv4_address     = join(".", [ local.LXD_NETWORK_TO_USE, "10" ])
 
 #   container_cloud-init = templatefile(
@@ -69,11 +87,13 @@
 # module "deploy-<COMPONENT_NAME>" {
 #   source = "./modules/deploy-container-dynamic-ip"
 
+#   depends_on = [ module.deploy-consul ]
+
 #   lxd_remote                 = local.lxd_remote_name
 #   container_image            = join("-", [ local.project_id, "COMPONENT_NAME", var.image_version ])
 #   container_name             = join("-", [ local.project_id, "COMPONENT_NAME" ])
 #   container_profiles         = ["default"]
-#   container_network          = "LXD NETWORK TO USE"
+#   container_network          = local.project_id
 
 #   container_cloud-init       = file("cloud-init/cloud-TEMPLATE.yml")
 
@@ -96,11 +116,13 @@
 # module "deploy-<COMPONENT_NAME>" {
 #   source = "./modules/deploy-container-dynamic-ip"
 
+#   depends_on = [ module.deploy-consul ]
+
 #   lxd_remote                 = local.lxd_remote_name
 #   container_image            = join("-", [ local.project_id, "COMPONENT_NAME", var.image_version ])
 #   container_name             = join("-", [ local.project_id, "COMPONENT_NAME" ])
 #   container_profiles         = ["default"]
-#   container_network          = "LXD NETWORK TO USE"
+#   container_network          = local.project_id
 
 #   container_cloud-init = templatefile(
 #     "cloud-init/cloud-init-<COMPONENT_NAME>.tpl.yml",
@@ -119,4 +141,40 @@
 #   container_mounts = [
 #     {name = "<NAME>", host_path = "/var/containers/<PROJECT_ID>/<SUBDIRECTORY>", mount_path = "<PATH>", mount_readonly = true}
 #   ]
+# }
+
+
+# Deployment of load balancer / TLS proxy configuration for <COMPONENT_NAME>
+############################################################################
+
+# module "deploy-<COMPONENT_NAME>-haproxy-backend-service" {
+#   source = "./modules/deploy-haproxy-backend-services"
+
+#   depends_on = [ module.deploy-consul ]
+
+#   non_ssl_backend_services = [ join("-", [ local.project_id, "<COMPONENT_NAME>" ]) ]
+# }
+
+
+# module "deploy-<COMPONENT_NAME>-haproxy-acl-configuration" {
+#   source = "./modules/deploy-haproxy-configuration"
+
+#   depends_on = [ module.deploy-<COMPONENT_NAME>-haproxy-backend-service ]
+
+#   haproxy_host_only_acls = {
+#     domain     = {host = local.project_domain_name},
+#     domain-www = {host = join("", [ "www.", local.project_domain_name])}
+#   }
+# }
+
+
+# module "deploy-<COMPONENT_NAME>-haproxy-backend-configuration" {
+#   source = "./modules/deploy-haproxy-configuration"
+
+#   depends_on = [ module.deploy-<COMPONENT_NAME>-haproxy-backend-service ]
+
+#   haproxy_acl_use-backends = {
+#     domain     = {backend_service = join("-", [ local.project_id, "<COMPONENT_NAME>" ])},
+#     domain-www = {backend_service = join("-", [ local.project_id, "<COMPONENT_NAME>" ])}
+#   }
 # }
